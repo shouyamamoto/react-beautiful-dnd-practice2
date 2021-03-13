e075b27dff92a28a1fda50269e5ede4bc1c8b9e4
shouyamamoto <yamasyo0017@gmail.com>
2021-03-13 13:04:37 +0900

    set up

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => 'Hello World'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
