1ebd01e060587564db60c788f25bb920ce6b7089
shouyamamoto <yamasyo0017@gmail.com>
2021-03-14 06:30:38 +0900

    add dragHandleProps

--- src/task.jsx
+++ src/task.jsx
@@ -8,7 +8,16 @@ const Container = styled.li`
   padding: 8px;
   margin-bottom: 8px;
   list-style: none;
+  display: flex;
   background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
+`
+
+const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: orange;
+  border-radius: 4px;
+  margin-right: 8px;
 `
 
 export default class Task extends React.Component {
@@ -18,10 +27,12 @@ export default class Task extends React.Component {
         {(provided, snapshot) => (
           <Container
             {...provided.draggableProps}
-            {...provided.dragHandleProps}
             ref={provided.innerRef}
             isDragging={snapshot.isDragging}
           >
+          <Handle 
+            {...provided.dragHandleProps}
+          />
           {this.props.task.content}
           </Container>
         )}
